c9e796a6e2663880c08b64aebadf3019a8a56182
Kisaragi Hiu <mail@kisaragi-hiu.com>
2022-10-19 10:41:08 +0900

    A miserable Hugo test site
    Using Hugo implies processing the JSON using Go Template. Turning
    "{[abcd]}" into the corresponding character "\uABCD"? Nesting things a
    little? Good luck!
    
    It's probably worth it to try to learn Eleventy at this point.

---
+++ test-site/tailwind.config.js
@@ -0,0 +1,45 @@
+let cjkFallback = [
+  "Noto Sans CJK TC",
+  "Microsoft Jhenghei",
+  "Microsoft Yahei",
+  "Meiryo",
+  "Malgun Gothic",
+];
+
+module.exports = {
+  // darkMode: "class",
+  content: ["./layouts/**/*.html", "./content/**/*.md"],
+  theme: {
+    fontFamily: {
+      // This should be called "default".
+      sans: [
+        "'Equity A'",
+        "'Noto Serif CJK JP'",
+        "'Fira Sans'",
+        ...cjkFallback,
+        "sans-serif",
+      ],
+      "sans-serif": [
+        "'Fira Sans'",
+        "'Noto Sans CJK TW'",
+        ...cjkFallback,
+        "sans-serif",
+      ],
+      // serif: ["Equity"],
+      mono: [
+        "Inconsolata",
+        "Noto Sans Mono CJK TC",
+        ...cjkFallback,
+        "monospace",
+      ],
+    },
+    extend: {
+      // https://github.com/tailwindlabs/tailwindcss/discussions/1361
+      boxShadow: {
+        DEFAULT: "0 0 0.25rem #00000040",
+        md: "0 0 0.25rem #00000070",
+        white: "0 0 0.5rem #ffffff",
+      },
+    },
+  },
+};
--- testing.el
+++ testing.el
@@ -5,7 +5,20 @@
 (with-temp-buffer
   (insert-file-contents "moedict-data/dict-revised.json")
   (goto-char (point-min))
-  (setq kemdict/dict-revised.json (json-parse-buffer)))
+  (setq kemdict/dict-revised.json
+        (json-parse-buffer :object-type 'alist))
+  nil)
+
+(let ((n 0))
+  (catch 'ret
+    (while t
+      (let-alist (elt kemdict/dict-revised.json n)
+        (when (equal "向" .title)
+          (throw 'ret n))
+        (cl-incf n)))))
+
+(let-alist (elt kemdict/dict-revised.json 27046)
+  (elt .heteronyms 1))
 
 ;; Local Variables:
 ;; flycheck-disabled-checkers: (emacs-lisp-checkdoc)
