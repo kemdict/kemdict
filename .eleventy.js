7b75ce169890171f33b5aa12ce416830486b3665
Kisaragi Hiu <mail@kisaragi-hiu.com>
2022-10-27 16:04:32 +0900

    try to not use eleventyhtmlbase to maybe avoid an out of memory

--- .eleventy.js
+++ .eleventy.js
@@ -1,7 +1,4 @@
-const { EleventyHtmlBasePlugin } = require("@11ty/eleventy");
-
 module.exports = (cfg) => {
-  cfg.addPlugin(EleventyHtmlBasePlugin);
   cfg.addFilter("moedict_zh_process_def", (def) => {
     return def.replace(/「(.*?)」/g, "「<a href='/word/$1'>$1</a>」");
   });
