26701d4b956861b43716100a3680c9c010a318dc
Kisaragi Hiu <mail@kisaragi-hiu.com>
2022-11-01 18:54:42 +0900

    dictionaries: Reform Makefile; generate kisaragi_dict.json on build
    ...as opposed to having to remember to C-c C-c (re-run) an Org source
    block after editing.

--- Makefile
+++ Makefile
@@ -4,11 +4,47 @@ export LANG=en_US.UTF-8
 
 .PHONY: dev-11ty dev-tailwind dev process-data
 
+# * Processing dictionary data into one combined file
+src/_data: src/_data/combined.json kisaragi-dict/kisaragi_dict.json
+
+src/_data/combined.json: moedict-data/dict-revised.json moedict-data-twblg/dict-twblg.json kisaragi-dict/kisaragi_dict.json process-data.el
+	cask eval "(load \"process-data\")"
+
+src/_data/kisaragi_dict.json: kisaragi-dict/kisaragi_dict.json
+	cp kisaragi-dict/kisaragi_dict.json src/_data/
+
+kisaragi-dict/kisaragi_dict.json: kisaragi-dict/kisaragi-dict.org kisaragi-dict/generate.el
+	cask eval "(load \"kisaragi-dict/generate\")"
+
+moedict-data/dict-revised.json:
+	git submodule update --init -- moedict-data
+
+moedict-data-twblg/dict-twblg.json:
+	git submodule update --init -- moedict-data-twblg
+
+# * Development tasks
 dev-11ty:
 	npx "@11ty/eleventy" --input=./src --output=./_site --serve
 
 dev-tailwind:
 	npx tailwindcss --postcss -i css/src.css -o _site/b.css --watch
+
+dev: _site/s.js
+	export DEV=true
+	make src/_data
+	npx concurrently "make dev-11ty" "make dev-tailwind"
+
+# * Making the site itself
+_site: src/*.njk src/_data _site/b.css _site/s.js .eleventy.js
+	npx "@11ty/eleventy" --quiet
+
+gh-pages: src/_data _site/b.css
+	npx "@11ty/eleventy" --pathprefix "kemdict" --quiet
+
+build: _site
+
+public.zip: _site
+	cd _site/ && 7z a ../public.zip .
 
 _site/b.css: css/src.css
 	npx tailwindcss --minify --postcss -i css/src.css -o _site/b.css
@@ -16,27 +52,3 @@ _site/b.css: css/src.css
 _site/%.js: client-side-js/%.ts .babelrc.json
 	npx babel "$<" --out-file "$@"
 
-_site: src/*.njk src/_data _site/b.css _site/s.js .eleventy.js
-	npx "@11ty/eleventy" --quiet
-
-gh-pages: src/_data _site/b.css
-	npx "@11ty/eleventy" --pathprefix "kemdict" --quiet
-
-src/_data: src/_data/combined.json kisaragi-dict/kisaragi_dict.json
-	cp kisaragi-dict/kisaragi_dict.json src/_data/
-
-src/_data/combined.json: moedict-data/dict-revised.json moedict-data-twblg/dict-twblg.json kisaragi-dict/kisaragi_dict.json process-data.el
-	make process-data
-
-process-data:
-	cask eval "(load \"process-data\")"
-
-dev: _site/s.js
-	export DEV=true
-	make src/_data
-	npx concurrently "make dev-11ty" "make dev-tailwind"
-
-build: _site
-
-public.zip: _site
-	cd _site/ && 7z a ../public.zip .
---
+++ kisaragi-dict/generate.el
@@ -0,0 +1,95 @@
+;;; generate.el --- Process kisaragi-dict.org into JSON -*- lexical-binding: t -*-
+
+;; Author: Kisaragi Hiu
+
+;;; Commentary:
+
+;; kisaragi-dict.org's format is described within. This file
+;; implements the format itself.
+;;
+;; Essentially entries are headings under the "Words" heading,
+;; structured in a particular way, and this file converts that to JSON
+;; in a schema similar to that used by Moedict.
+
+;;; Code:
+
+(require 'cl-lib)
+(require 'org-element)
+(require 'json)
+(require 'dash)
+
+(when load-file-name
+  (setq default-directory (file-name-directory load-file-name)))
+
+(defun kisaragi-dict/elem-title (elem)
+  "Return the only title of ELEM."
+  (org-no-properties
+   (org-element-interpret-data
+    (org-element-property :title elem))))
+
+(defun kisaragi-dict/elements-to-json (elems)
+  "Process ELEMS to JSON for kisaragi-dict."
+  (cl-loop
+   for elem in (-sort (-on #'string< #'kisaragi-dict/elem-title) elems)
+   collect
+   ;; title
+   (list
+    (cons "title" (kisaragi-dict/elem-title elem))
+    (cons "heteronyms"
+          (cl-loop
+           for het in (org-element-contents elem)
+           collect
+           ;; pronunciation
+           (list (cons "pronunciation" (kisaragi-dict/elem-title het))
+                 (cons "definitions"
+                       (cl-loop
+                        for definition in (org-element-contents het)
+                        collect
+                        (let* ((type+def
+                                (split-string (kisaragi-dict/elem-title definition) "|"))
+                               (def
+                                (list (cons "type" (elt type+def 0))
+                                      (cons "def" (elt type+def 1)))))
+                          (dolist (prop-headline (org-element-contents definition))
+                            (pcase (kisaragi-dict/elem-title prop-headline)
+                              ("例"
+                               (push (cons "example"
+                                           (string-trim
+                                            (org-element-interpret-data
+                                             (org-element-contents prop-headline))))
+                                     def))
+                              ("語源"
+                               (push (cons "etymology"
+                                           (string-trim
+                                            (org-element-interpret-data
+                                             (org-element-contents prop-headline))))
+                                     def))))
+                          def)))))))))
+
+(defun kisaragi-dict/parse-elements ()
+  "Parse elements in this buffer."
+  (with-temp-buffer
+    (insert-file-contents "kisaragi-dict.org")
+    (org-mode)
+    (goto-char (point-min))
+    (re-search-forward (rx bol "* Words") nil t)
+    (forward-line)
+    (beginning-of-line)
+    (narrow-to-region (point) (point-max))
+    (org-element-contents (org-element-parse-buffer))))
+
+(when (and (fboundp #'native-comp-available-p)
+           (native-comp-available-p))
+  (native-compile #'kisaragi-dict/elements-to-json)
+  (native-compile #'kisaragi-dict/parse-elements))
+
+(let ((json-encoding-pretty-print t))
+  (with-temp-file "kisaragi_dict.json"
+    (message "Generating kisaragi_dict.json...")
+    (insert (json-encode
+             (kisaragi-dict/elements-to-json
+              (kisaragi-dict/parse-elements)))
+            "\n")
+    (message "Generating kisaragi_dict.json...done")))
+
+;;; generate.el ends here
