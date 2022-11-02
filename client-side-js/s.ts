4516f2735c287553335080126a48a463ab09e612
Kisaragi Hiu <mail@kisaragi-hiu.com>
2022-11-03 02:18:07 +0900

    search: fix incorrect path
    On GitHub Pages ./ is "/word/<word>/" while on Netlify ./ is "/word/",
    so the relative path behavior is different.
    
    We've given up on using relative paths already. Just use a local
    absolute path.

--- client-side-js/s.ts
+++ client-side-js/s.ts
@@ -102,21 +102,7 @@ if (searchForm) {
     let word = searchBar.value;
     event.preventDefault();
     if (word.trim() !== "") {
-      // HACK around the fact that we can't access Eleventy's
-      // pathprefix from here.
-      //
-      // We could put this file in a template (like s.js.njk), except
-      // then we'd have to specify the right path in the permalink
-      // prop, as part of the front matter. The front matter syntax is
-      // not valid JS and will cause editing tools to barf; it can be
-      // customized, but that customization is global across the entire
-      // project.
-      // I'll just do this instead.
-      if (location.href.indexOf("word") == -1) {
-        location.href = "word/" + word;
-      } else {
-        location.href = "" + word;
-      }
+      location.href = "/word/" + word;
     }
   });
 }
