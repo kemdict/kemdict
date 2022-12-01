4f37b8a48989c07c601781c7bd4ce92c9881c34f
Kisaragi Hiu <mail@kisaragi-hiu.com>
2022-12-01 19:21:51 +0900

    Save run time memory by connecting to uncompressed db directly
    This way we don't have to have an in-memory database.

--- web/src/lib/server/db.js
+++ web/src/lib/server/db.js
@@ -3,32 +3,35 @@ import * as zlib from "node:zlib";
 import Database from "better-sqlite3";
 
 /**
- * Read a brotli-compressed SQLite database and return a Database
+ * Read a gzipped SQLite database and return a Database
  * object for it as an in-memory database.
+ * If `path` does not end in ".gz", try to return a connection instead
+ * (without using an in-memory database).
  * @param {string} path
  * @returns {Database}
  */
 function readDB(path) {
-  let data = fs.readFileSync(path);
-  let decompressed = zlib.gunzipSync(data);
-  return new Database(decompressed);
+  if (path.endsWith(".db.gz")) {
+    let data = fs.readFileSync(path);
+    let decompressed = zlib.gunzipSync(data);
+    return new Database(decompressed);
+  } else {
+    return new Database(path);
+  }
 }
 
 export const db = (() => {
-  // If we build on Netlify, like the path copying doesn't work well
-  // with the project being in a subdirectory, ie. during build we get
-  // src/... but during serverless function runtime we get web/src/...
-  //
-  // Just, like, work around that.
-  try {
-    return readDB("./src/lib/entries.db.gz");
-  } catch (e) {
-    if (e instanceof Error && e.code === "ENOENT") {
-      return readDB("./web/src/lib/entries.db.gz");
-    } else {
-      throw e;
-    }
-  }
+  let path = [
+    "./src/lib/entries.db",
+    "./src/lib/entries.db.gz",
+    // If we build on Netlify, like the path copying doesn't work well
+    // with the project being in a subdirectory, ie. during build we get
+    // src/... but during serverless function runtime we get web/src/...
+    //
+    // Just, like, work around that.
+    "./web/src/lib/entries.db.gz",
+  ].find((f) => fs.existsSync(f));
+  return readDB(path);
 })();
 
 /**
