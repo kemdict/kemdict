6165e128406e2641e0a3d48814296179ccff6844
Kisaragi Hiu <mail@kisaragi-hiu.com>
2023-01-04 04:35:47 +0900

    Apply out link in more places

--- web/src/lib/components/Elsewhere.svelte
+++ web/src/lib/components/Elsewhere.svelte
@@ -1,26 +1,30 @@
 <script>
   export let term;
+
+  import Out from "./Out.svelte";
 </script>
 
 <div class="prose">
   <ul class="my-10">
     <li>
-      <a href="https://google.com/search?q={term}">在 Google 搜尋「{term}」</a>
-    </li>
-    <li>
-      <a href="https://zh.wiktionary.org/w/index.php?search={term}"
-        >在維基詞典搜尋「{term}」</a
+      <Out href="https://google.com/search?q={term}"
+        >在 Google 搜尋「{term}」</Out
       >
     </li>
     <li>
-      <a
+      <Out href="https://zh.wiktionary.org/w/index.php?search={term}"
+        >在維基詞典搜尋「{term}」</Out
+      >
+    </li>
+    <li>
+      <Out
         href="https://terms.naer.edu.tw/search/?match_type=phrase&query_op=&query_field=title&query_term={term}"
-        >在學術名詞資料庫搜尋「{term}」</a
+        >在學術名詞資料庫搜尋「{term}」</Out
       >
     </li>
     <li>
-      <a href="https://www.weblio.jp/content_find?query={term}"
-        >在 Weblio 搜尋「{term}」</a
+      <Out href="https://www.weblio.jp/content_find?query={term}"
+        >在 Weblio 搜尋「{term}」</Out
       >
     </li>
   </ul>
--- web/src/routes/word/[word]/+page.svelte
+++ web/src/routes/word/[word]/+page.svelte
@@ -4,6 +4,7 @@
   import WordList from "$lib/components/WordList.svelte";
   import SearchBar from "$lib/components/SearchBar.svelte";
   import Header from "$lib/components/Header.svelte";
+  import Out from "$lib/components/Out.svelte";
   export let data;
   $: word = data.word;
   $: backlinks = data.backlinks;
@@ -32,29 +33,31 @@
   <h2>在其他地方搜尋「{word.title}」</h2>
   <ul>
     <li>
-      <a href="https://google.com/search?q={word.title}">Google</a>
+      <Out href="https://google.com/search?q={word.title}">Google</Out>
     </li>
     <li>
-      <a href="https://zh.wikipedia.org/w/index.php?search={word.title}"
-        >維基百科</a
+      <Out href="https://zh.wikipedia.org/w/index.php?search={word.title}"
+        >維基百科</Out
       >
     </li>
     <li>
-      <a href="https://zh.wiktionary.org/w/index.php?search={word.title}"
-        >維基詞典</a
+      <Out href="https://zh.wiktionary.org/w/index.php?search={word.title}"
+        >維基詞典</Out
       >
     </li>
     <li>
-      <a href="https://moedict.tw/{word.title}">萌典</a>
+      <Out href="https://moedict.tw/{word.title}">萌典</Out>
     </li>
     <li>
-      <a
+      <Out
         href="https://terms.naer.edu.tw/search/?match_type=phrase&query_op=&query_field=title&query_term={word.title}"
-        >學術名詞資料庫</a
+        >學術名詞資料庫</Out
       >
     </li>
     <li>
-      <a href="https://www.weblio.jp/content_find?query={word.title}">Weblio</a>
+      <Out href="https://www.weblio.jp/content_find?query={word.title}"
+        >Weblio</Out
+      >
     </li>
   </ul>
   {#if word.hakkadict || word.dict_revised || word.moedict_twblg || word.dict_concised || word.dict_idioms}
@@ -65,7 +68,7 @@
     </p>
     {#if word.dict_concised}
       <p>
-        《<a href="https://dict.concised.moe.edu.tw">國語辭典簡編本</a>》©
+        《<Out href="https://dict.concised.moe.edu.tw">國語辭典簡編本</Out>》©
         中華民國教育部（版本編號：2014_20220928，<a
           href="/l/concisedict_10312.pdf">使用說明</a
         >）
@@ -73,30 +76,30 @@
     {/if}
     {#if word.dict_revised}
       <p>
-        《<a href="https://dict.revised.moe.edu.tw">重編國語辭典修訂本</a>》©
-        中華民國教育部（版本編號：2015_20220922，<a
+        《<Out href="https://dict.revised.moe.edu.tw">重編國語辭典修訂本</Out
+        >》© 中華民國教育部（版本編號：2015_20220922，<a
           href="/l/reviseddict_10312.pdf">使用說明</a
         >）
       </p>
     {/if}
     {#if word.moedict_twblg}
       <p>
-        《<a href="https://twblg.dict.edu.tw">臺灣閩南語常用詞辭典</a>》©
+        《<Out href="https://twblg.dict.edu.tw">臺灣閩南語常用詞辭典</Out>》©
         中華民國教育部（版本編號：20190610，見
-        <a href="https://github.com/g0v/moedict-data-twblg"
-          >moedict-data-twblg</a
+        <Out href="https://github.com/g0v/moedict-data-twblg"
+          >moedict-data-twblg</Out
         >）
       </p>
     {/if}
     {#if word.hakkadict}
       <p>
-        《<a href="https://hakkadict.moe.edu.tw">臺灣客家語常用詞辭典</a>》©
+        《<Out href="https://hakkadict.moe.edu.tw">臺灣客家語常用詞辭典</Out>》©
         中華民國教育部（版本編號：1100429）
       </p>
     {/if}
     {#if word.dict_idioms}
       <p>
-        《<a href="https://dict.idioms.moe.edu.tw">成語典</a>》©
+        《<Out href="https://dict.idioms.moe.edu.tw">成語典</Out>》©
         中華民國教育部（版本編號：2020_20220928，<a
           href="/l/idiomsdict_10409.pdf">使用說明</a
         >）
@@ -107,7 +110,7 @@
 
 <hr />
 <p class="prose">
-  如果 Kemdict 有幫助到您，請考慮<a
-    href="https://www.buymeacoffee.com/kisaragihiu">贊助我一餐飯</a
+  如果 Kemdict 有幫助到您，請考慮<Out
+    href="https://www.buymeacoffee.com/kisaragihiu">贊助我一餐飯</Out
   >。謝謝。
 </p>
