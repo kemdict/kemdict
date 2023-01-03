f352c2ad9d70408dc5f2e018e69228eafe1f74f5
Kisaragi Hiu <mail@kisaragi-hiu.com>
2023-01-04 02:44:33 +0900

    Add the loading indicator back

--- web/src/routes/+layout.svelte
+++ web/src/routes/+layout.svelte
@@ -1,5 +1,14 @@
 <script>
   import "../src.css";
+
+  import { navigating } from "$app/stores";
+  import { fade } from "svelte/transition";
+  import Loading from "./Loading.svelte";
 </script>
 
+{#if $navigating}
+  <div out:fade={{ delay: 250 }}>
+    <Loading />
+  </div>
+{/if}
 <slot />
