b568e6ef6ea374171fca568855208e9ce1ca039e
Kisaragi Hiu <mail@kisaragi-hiu.com>
2023-01-05 02:58:21 +0900

    don't load .md files as svelte components; add routes/README.md
    routes/README.md is a source code explanation document.

---
+++ web/src/routes/README.md
@@ -0,0 +1,5 @@
+# Routes
+
+SvelteKit pages and settings.
+
+During build, Make has been set up to copy `/pages/<filename>.md` to `/src/routes/<filename>/+page.svelte.md`, so that `/pages/abc.md` becomes the `/abc` route. This is just to make articles easier to edit.
--- web/svelte.config.js
+++ web/svelte.config.js
@@ -3,14 +3,14 @@ import adapterNode from "@sveltejs/adapter-node";
 import preprocess from "svelte-preprocess";
 
 const config = {
-  extensions: [".svelte", ".svelte.md", ".md", ".svx"],
+  extensions: [".svelte", ".svelte.md", ".svx"],
   preprocess: [
     preprocess(),
     mdsvex({
       smartypants: {
         dashes: "oldschool",
       },
-      extensions: [".svelte.md", ".md", ".svx"],
+      extensions: [".svelte.md", ".svx"],
       layout: "src/lib/MarkdownLayout.svelte",
     }),
   ],
