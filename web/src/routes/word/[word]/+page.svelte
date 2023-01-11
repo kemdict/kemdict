59d4ac93a3f55bb8e7ec12155336ec0b741a87cb
Kisaragi Hiu <mail@kisaragi-hiu.com>
2023-01-11 18:32:26 +0800

    Merge Word into word page, its only use

--- web/src/routes/word/[word]/+page.svelte
+++ web/src/routes/word/[word]/+page.svelte
@@ -2,14 +2,20 @@
   /** @type {import('./$types').PageData} */
   export let data;
 
+  import SplitLayout from "$lib/SplitLayout.svelte";
+
   import Elsewhere from "$lib/Elsewhere.svelte";
   import Out from "$lib/Out.svelte";
-  import SplitLayout from "$lib/SplitLayout.svelte";
   import TOC from "$lib/TOC.svelte";
-  import Word from "$lib/Word.svelte";
+  import WordDictConcised from "$lib/WordDictConcised.svelte";
+  import WordDictIdioms from "$lib/WordDictIdioms.svelte";
+  import WordDictRevised from "$lib/WordDictRevised.svelte";
+  import WordHakkadict from "$lib/WordHakkadict.svelte";
+  import WordITaigi from "$lib/WordITaigi.svelte";
   import WordList from "$lib/WordList.svelte";
+  import WordMoedictish from "$lib/WordMoedictish.svelte";
 
-  import { dictsInWord } from "$lib/common";
+  import { dictsInWord, format, groupByProp } from "$lib/common";
 
   $: word = data.word;
   $: backlinks = data.backlinks;
@@ -36,7 +42,37 @@
   </div>
   <svelte:fragment slot="right">
     <div class="sm:hidden"><TOC term={word.title} {presentDicts} /></div>
-    <Word {word} />
+    {#each groupByProp(presentDicts, "lang") as [lang, dicts]}
+      {#each dicts as dict}
+        <div id={dict.id} class="dict">
+          <Out href={format(dict.url, word.title)}>{dict.name}</Out>
+          <span
+            ><a
+              class="hover:link dark:gray-500 text-gray-400"
+              href="#{dict.id}"
+              id={dict.id}>#</a
+            ></span
+          >
+        </div>
+        <div class="word">
+          {#if dict.id == "kisaragi_dict"}
+            <WordMoedictish {word} dict={dict.id} />
+          {:else if dict.id == "chhoetaigi_itaigi"}
+            <WordITaigi entry={word[dict.id]} />
+          {:else if dict.id == "dict_revised"}
+            <WordDictRevised entry={word[dict.id]} title={word.title} />
+          {:else if dict.id == "dict_concised"}
+            <WordDictConcised entry={word[dict.id]} title={word.title} />
+          {:else if dict.id == "moedict_twblg"}
+            <WordMoedictish {word} dict={dict.id} />
+          {:else if dict.id == "hakkadict"}
+            <WordHakkadict entry={word[dict.id]} title={word.title} />
+          {:else if dict.id == "dict_idioms"}
+            <WordDictIdioms entry={word[dict.id]} title={word.title} />
+          {/if}
+        </div>
+      {/each}
+    {/each}
   </svelte:fragment>
 </SplitLayout>
 
