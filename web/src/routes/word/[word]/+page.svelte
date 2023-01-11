82540c2e1e9de7b70a65d6e9d6d748c954077d3f
Kisaragi Hiu <mail@kisaragi-hiu.com>
2023-01-11 18:50:03 +0800

    Display copyright immediately after each dict block

--- web/src/routes/word/[word]/+page.svelte
+++ web/src/routes/word/[word]/+page.svelte
@@ -28,7 +28,7 @@
 </svelte:head>
 
 <SplitLayout leftFirst={false} initialInput={word.title}>
-  <div slot="left">
+  <div class="sm:fixed" slot="left">
     <div class="max-sm:hidden">
       <TOC {presentDicts} />
     </div>
@@ -61,72 +61,61 @@
             <WordITaigi entry={word[dict.id]} />
           {:else if dict.id == "dict_revised"}
             <WordDictRevised entry={word[dict.id]} title={word.title} />
+            <div class="copyright">
+              <p>
+                《<a href="https://dict.revised.moe.edu.tw/"
+                  >重編國語辭典修訂本</a
+                >》版本編號：2015_20220922
+              </p>
+              <p>© 中華民國教育部 (Ministry of Education, R.O.C.)</p>
+              <p><a href="/l/reviseddict_10312.pdf">使用說明</a></p>
+            </div>
           {:else if dict.id == "dict_concised"}
             <WordDictConcised entry={word[dict.id]} title={word.title} />
+            <div class="copyright">
+              <p>
+                《<a href="https://dict.concised.moe.edu.tw">國語辭典簡編本</a
+                >》版本編號：2014_20220928
+              </p>
+              <p>© 中華民國教育部 (Ministry of Education, R.O.C.)</p>
+              <p><a href="/l/conciseddict_10312.pdf">使用說明</a></p>
+            </div>
           {:else if dict.id == "moedict_twblg"}
             <WordMoedictish {word} dict={dict.id} />
+            <div class="copyright">
+              <p>
+                《<a href="https://twblg.dict.edu.tw">臺灣閩南語常用詞辭典</a
+                >》版本編號：20190610
+              </p>
+              <p>© 中華民國教育部 (Ministry of Education, R.O.C.)</p>
+              <p>
+                資料來自<a href="https://github.com/g0v/moedict-data-twblg"
+                  >moedict-data-twblg</a
+                >
+              </p>
+            </div>
           {:else if dict.id == "hakkadict"}
             <WordHakkadict entry={word[dict.id]} title={word.title} />
+            <div class="copyright">
+              <p>
+                《<a href="https://hakkadict.moe.edu.tw">臺灣客家語常用詞辭典</a
+                >》版本編號：1100429
+              </p>
+              <p>© 中華民國教育部 (Ministry of Education, R.O.C.)</p>
+            </div>
           {:else if dict.id == "dict_idioms"}
             <WordDictIdioms entry={word[dict.id]} title={word.title} />
+            <div class="copyright">
+              <p>
+                《<a href="https://dict.idioms.moe.edu.tw">成語典</a
+                >》版本編號：2020_20220928
+              </p>
+              <p>© 中華民國教育部 (Ministry of Education, R.O.C.)</p>
+              <p><a href="/l/idiomsdict_10409.pdf">使用說明</a></p>
+            </div>
           {/if}
         </div>
       {/each}
     {/each}
   </svelte:fragment>
 </SplitLayout>
-
-<div class="prose mt-8">
-  {#if presentDicts.length > 1 || presentDicts[0].id !== "kisaragi_dict"}
-    <h2 class="mt-2">本頁的著作權</h2>
-    <p>
-      《重編國語辭典修訂本》、《國語辭典簡編本》、《成語典》、《臺灣客家語常用詞辭典》與《臺灣閩南語常用詞辭典》為中華民國教育部版權所有，依「創用CC-姓名標示-禁止改作
-      3.0 臺灣授權條款」釋出。
-    </p>
-    {#if word.dict_concised}
-      <p>
-        《<Out href="https://dict.concised.moe.edu.tw">國語辭典簡編本</Out>》©
-        中華民國教育部（版本編號：2014_20220928，<a
-          href="/l/conciseddict_10312.pdf">使用說明</a
-        >）
-      </p>
-    {/if}
-    {#if word.dict_revised}
-      <p>
-        《<Out href="https://dict.revised.moe.edu.tw">重編國語辭典修訂本</Out
-        >》© 中華民國教育部（版本編號：2015_20220922，<a
-          href="/l/reviseddict_10312.pdf">使用說明</a
-        >）
-      </p>
-    {/if}
-    {#if word.moedict_twblg}
-      <p>
-        《<Out href="https://twblg.dict.edu.tw">臺灣閩南語常用詞辭典</Out>》©
-        中華民國教育部（版本編號：20190610，見
-        <Out href="https://github.com/g0v/moedict-data-twblg"
-          >moedict-data-twblg</Out
-        >）
-      </p>
-    {/if}
-    {#if word.hakkadict}
-      <p>
-        《<Out href="https://hakkadict.moe.edu.tw">臺灣客家語常用詞辭典</Out>》©
-        中華民國教育部（版本編號：1100429）
-      </p>
-    {/if}
-    {#if word.dict_idioms}
-      <p>
-        《<Out href="https://dict.idioms.moe.edu.tw">成語典</Out>》©
-        中華民國教育部（版本編號：2020_20220928，<a
-          href="/l/idiomsdict_10409.pdf">使用說明</a
-        >）
-      </p>
-    {/if}
-  {/if}
-  <hr />
-  <p class="prose">
-    如果 Kemdict 有幫助到您，請考慮<Out
-      href="https://www.buymeacoffee.com/kisaragihiu">贊助我一餐飯</Out
-    >。謝謝。
-  </p>
-</div>
