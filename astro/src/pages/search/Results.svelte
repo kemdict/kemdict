7dc7f7c873ace1924376196167bd5e67df1ffb8f
Kisaragi Hiu <mail@kisaragi-hiu.com>
2023-03-05 19:31:22 +0900

    Tabs in search

---
+++ astro/src/pages/search/Results.svelte
@@ -0,0 +1,29 @@
+<script>
+  export let langs = ["id", "readablename"];
+  export let heteronyms = [];
+  import { dictsByLang } from "$src/common";
+  import { Tabs, Tab, TabList, TabPanel } from "svelte-tabs";
+  import WordPreview from "$src/components/WordPreview.svelte";
+  console.log(langs);
+</script>
+
+<div class="mt-4">
+  <Tabs>
+    <TabList>
+      {#each langs as [langId, lang]}
+        <Tab>{lang}</Tab>
+      {/each}
+    </TabList>
+    {#each langs as [langId, lang]}
+      <TabPanel>
+        <ul>
+          {#each heteronyms as het}
+            {#if dictsByLang[langId].includes(het.from)}
+              <WordPreview heteronyms={[het]} />
+            {/if}
+          {/each}
+        </ul>
+      </TabPanel>
+    {/each}
+  </Tabs>
+</div>
