c53cc6e272f77bb4f267b0ff08a312cd7e2a7862
Kisaragi Hiu <mail@kisaragi-hiu.com>
2023-03-08 02:11:05 +0900

    Radical pages: group by stroke count

--- web/src/components/Group.svelte
+++ web/src/components/Group.svelte
@@ -1,14 +1,20 @@
 <script lang="ts">
-  export let title: string;
+  export let title: string | undefined;
   export let elements = [];
   export let template = "$1";
   import { format } from "$src/common";
 </script>
 
-<h2 class="mb-2">{title}</h2>
+<h2 class="mb-2 text-xl">
+  {#if title}
+    {title}
+  {:else}
+    其他
+  {/if}
+</h2>
 <ul class="flex list-none flex-wrap gap-x-2 p-0">
   {#each elements as element}
-    <li class="w-4">
+    <li class="w-4 text-lg max-sm:my-1 max-sm:text-xl">
       <a href={format(template, element.title || element)}
         >{element.title || element}</a
       >
