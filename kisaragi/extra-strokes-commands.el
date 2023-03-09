4a52aab940bb2a79068ead47752e4d7941592a2a
Kisaragi Hiu <mail@kisaragi-hiu.com>
2023-03-09 09:24:30 +0900

    new words

--- kisaragi/extra-strokes-commands.el
+++ kisaragi/extra-strokes-commands.el
@@ -4,19 +4,22 @@
 (defun k/add-new-entry ()
   ""
   (interactive)
-  (let ((input (read-string "title|radical|stroke|non-rad: ")))
-    (-let (((a c b d) (s-split "|" input)))
-      (goto-char (point-max))
-      (forward-line -2)
-      (end-of-line)
-      (insert (s-lex-format "
-,{
+  (catch 'exit
+    (while t
+      (let ((input (read-string "title,radical,stroke,non-rad: ")))
+        (when (s-blank? (s-trim input))
+          (throw 'exit nil))
+        (-let (((a c b d) (s-split "," input)))
+          (goto-char (point-max))
+          (forward-line -2)
+          (end-of-line)
+          (insert (s-lex-format ",{
 \"title\": \"${a}\",
 \"stroke_count\": ${b},
 \"radical\": \"${c}\",
 \"non_radical_stroke_count\": ${d}
-}
-")))))
+}")))
+        (save-buffer)))))
 
 ;; Local Variables:
 ;; mode: lisp-interaction
