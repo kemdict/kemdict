0325788daaf66a6c71e03ccc5c965e73964be0f0
Kisaragi Hiu <mail@kisaragi-hiu.com>
2023-03-22 23:53:49 +0900

    Fix importing astro component from Svelte

---
+++ web/src/components/Out.svelte
@@ -0,0 +1,20 @@
+<script>
+  /* Like Out.astro but usable in Svelte.
+
+     klass: classes for the anchor element
+     id: id of the anchor element
+     href: where the link goes
+   */
+  export let href = undefined;
+  let klass = undefined;
+  export { klass as class };
+  export let id = undefined;
+</script>
+
+<a
+  {href}
+  {id}
+  class="link after:content-['°'] {klass}"
+  target="_blank"
+  rel="noreferrer"><slot /></a
+>
