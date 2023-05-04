dd898de65a99aac5d6a180de4c055bc53a296875
Kisaragi Hiu <mail@kisaragi-hiu.com>
2023-05-05 02:25:34 +0900

    web: ListLink: don't include an empty p if body isn't given

--- web/src/components/ListLink.svelte
+++ web/src/components/ListLink.svelte
@@ -5,14 +5,16 @@
 <li>
   <a {href}>
     <div
-      class="-mx-1 my-2 p-1 text-sm transition hover:bg-surface-50 dark:hover:bg-stone-800"
+      class="hover:bg-surface-50 -mx-1 my-2 p-1 text-sm transition dark:hover:bg-stone-800"
     >
       <h2 class="link font-bold hover:no-underline">
         <slot name="heading" />
       </h2>
-      <p class="truncate text-gray-500 dark:text-stone-300">
-        <slot name="body" />
-      </p>
+      {#if $$slots.body}
+        <p class="truncate text-gray-500 dark:text-stone-300">
+          <slot name="body" />
+        </p>
+      {/if}
     </div>
   </a>
 </li>
