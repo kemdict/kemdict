5cbab23c9d1cfcb5c340853c10abc59aa3d7ef5d
Kisaragi Hiu <mail@kisaragi-hiu.com>
2023-05-04 17:55:13 +0900

    some semblance of a mobile app

--- App.tsx
+++ App.tsx
@@ -1,74 +1,12 @@
 import { StatusBar } from "expo-status-bar";
-import { StyleSheet, Text, View, SafeAreaView, FlatList } from "react-native";
-import * as FS from "expo-file-system";
-import * as SQLite from "expo-sqlite";
-import { Asset } from "expo-asset";
-import { useEffect, useState } from "react";
-import useSWR from "swr";
+import { StyleSheet, SafeAreaView, FlatList, ScrollView } from "react-native";
 
-/**
- * Read the database.
- */
-async function readDB(): Promise<SQLite.WebSQLDatabase> {
-  let info: FS.FileInfo;
-  // TODO: verify integrity
-  // info = await FS.getInfoAsync(FS.documentDirectory + "SQLite/entries.db");
-  // if (info.exists) {
-  //   await FS.deleteAsync(FS.documentDirectory + "SQLite/entries.db");
-  // }
-  info = await FS.getInfoAsync(FS.documentDirectory + "SQLite");
-  if (!info.exists) {
-    await FS.makeDirectoryAsync(FS.documentDirectory + "SQLite");
-  }
-  info = await FS.getInfoAsync(FS.documentDirectory + "SQLite/entries.db");
-  if (!info.exists) {
-    console.log("downloading");
-    await FS.downloadAsync(
-      // require only works with static values. (On Hermes only, I guess??)
-      Asset.fromModule(require("./assets/entries.db")).uri,
-      FS.documentDirectory + "SQLite/entries.db"
-    );
-    console.log("done");
-  }
-  return SQLite.openDatabase("entries.db");
-}
+import Page from "./Page.tsx";
 
 export default function App() {
-  const { data, isLoading } = useSWR("dummy", async () => {
-    const db = await readDB();
-    return await new Promise((resolve) => {
-      db.transaction((tx) => {
-        tx.executeSql(
-          `select * from "heteronyms" limit 10;`,
-          [],
-          (_, resultSet) => {
-            resolve(resultSet.rows._array);
-          },
-          (_, _err) => {
-            resolve([{ title: "error" }]);
-          }
-        );
-      });
-    });
-  });
   return (
     <SafeAreaView style={styles.container}>
-      <Text style={styles.text}>Hello!</Text>
-      {isLoading ? (
-        <View>
-          <Text style={styles.text}>Loading</Text>
-        </View>
-      ) : (
-        <FlatList
-          data={data}
-          renderItem={({ item }) => (
-            <View>
-              <Text style={styles.text}>{JSON.stringify(item)}</Text>
-            </View>
-          )}
-          keyExtractor={(item) => item.title}
-        ></FlatList>
-      )}
+      <Page />
       <StatusBar style="auto" />
     </SafeAreaView>
   );
@@ -76,12 +14,12 @@ export default function App() {
 
 const styles = StyleSheet.create({
   container: {
-    flex: 1,
-    backgroundColor: "#0B0F14",
-    alignItems: "center",
-    justifyContent: "center",
+    // flex: 1,
+    // backgroundColor: "#0B0F14",
+    // alignItems: "center",
+    // justifyContent: "center",
   },
   text: {
-    color: "#fff",
+    // color: "#fff",
   },
 });
