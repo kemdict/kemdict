c20229991687dac73da4c140dd50becc8ffb02bf
Kisaragi Hiu <mail@kisaragi-hiu.com>
2023-05-12 17:17:09 +0900

    inline "Abb.svelte"
    The map function is just a normal function, able to have its own local
    variables and stuff. So there is no need to create a (poorly named)
    component for this.

--- web/src/components/ListLink.svelte
+++ web/src/components/ListLink.svelte
@@ -1,20 +1,24 @@
 <script>
   export let href;
+  import clsx from "clsx";
 </script>
 
 <li>
-  <a {href}>
-    <div
-      class="hover:bg-surface-50 -mx-1 my-2 p-1 text-sm transition dark:hover:bg-stone-800"
-    >
-      <h2 class="link font-bold hover:no-underline">
-        <slot name="heading" />
-      </h2>
-      {#if $$slots.body}
-        <p class="truncate text-gray-500 dark:text-stone-300">
-          <slot name="body" />
-        </p>
-      {/if}
-    </div>
+  <a
+    class={clsx(
+      "unstyled block rounded transition",
+      "hover:bg-surface-100 dark:hover:bg-stone-800",
+      "-mx-1 my-2 p-1"
+    )}
+    {href}
+  >
+    <h2 class="link font-bold hover:no-underline">
+      <slot name="heading" />
+    </h2>
+    {#if $$slots.body}
+      <p class="truncate text-gray-500 dark:text-stone-300">
+        <slot name="body" />
+      </p>
+    {/if}
   </a>
 </li>
