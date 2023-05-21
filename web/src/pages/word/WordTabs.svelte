c07421f215fb163952fec2e4d48f3eaa02bc6930
Kisaragi Hiu <mail@kisaragi-hiu.com>
2023-05-22 05:11:59 +0900

    Add menu if there are more than 7 languages

--- web/src/pages/word/WordTabs.svelte
+++ web/src/pages/word/WordTabs.svelte
@@ -12,16 +12,53 @@
     return "?" + currentParams.toString();
   }
 
-  import { Tab, TabGroup } from "@skeletonlabs/skeleton";
+  import {
+    popup,
+    Tab,
+    TabGroup,
+    ListBox,
+    ListBoxItem,
+  } from "@skeletonlabs/skeleton";
   import { langs } from "common";
 </script>
 
-<TabGroup>
-  {#each presentLangs as langId}
+<TabGroup padding="">
+  {#each presentLangs.slice(0, 7) as langId}
     <Tab group={requestedLang} name={langId} value={langId}>
       <a class="block px-4 py-2" href="/word/{title}{paramsToString(langId)}"
         >{langs[langId]}</a
       >
     </Tab>
   {/each}
+  {#if presentLangs.length > 7}
+    <div class="ml-2 flex items-center">
+      <button
+        class="btn btn-sm variant-filled"
+        type="button"
+        title="其他語言"
+        use:popup={{
+          event: "click",
+          target: "popupLangs",
+          placement: "bottom",
+          closeQuery: ".listbox-item",
+        }}
+        >{presentLangs.indexOf(requestedLang) >= 7
+          ? langs[requestedLang]
+          : ""}…</button
+      >
+      <div class="card z-10 p-4 shadow-xl" data-popup="popupLangs">
+        <ListBox padding="">
+          {#each presentLangs.slice(7) as langId}
+            <ListBoxItem group={requestedLang} name="lang" value={langId}>
+              <a
+                class="block px-4 py-2"
+                href="/word/{title}{paramsToString(langId)}">{langs[langId]}</a
+              >
+            </ListBoxItem>
+          {/each}
+        </ListBox>
+        <div class="arrow bg-surface-100-800-token" />
+      </div>
+    </div>
+  {/if}
 </TabGroup>
