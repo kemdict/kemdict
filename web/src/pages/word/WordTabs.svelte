d034081cee9841b667a76074d3a829ae0b28e4a0
Kisaragi Hiu <mail@kisaragi-hiu.com>
2023-05-22 04:51:13 +0900

    wordtabs: fix hydration error
    We cannot pass an URLSearchParams directly as it is serialized to {}
    by JSON.stringify. We have to serialize it ourselves with its
    toString method.

--- web/src/pages/word/WordTabs.svelte
+++ web/src/pages/word/WordTabs.svelte
@@ -1,36 +1,27 @@
 <script lang="ts">
   export let presentLangs: string[] = [];
   export let requestedLang: string;
-  export let groupedHets: [Dict, Heteronym[]][];
   export let title: string;
-  export let currentParams: URLSearchParams;
+  export let currentParamString: string;
+  const currentParams = new URLSearchParams(currentParamString);
 
-  function paramsToString(params: URLSearchParams, langId: string) {
-    params.set("lang", langId);
-    return "?" + params.toString();
+  function paramsToString(langId: string) {
+    if (currentParams) {
+      currentParams.set("lang", langId);
+    }
+    return "?" + currentParams.toString();
   }
 
   import { Tab, TabGroup } from "@skeletonlabs/skeleton";
   import { langs } from "common";
-  import type { Dict, Heteronym } from "common";
-  import Word from "./Word.svelte";
 </script>
 
 <TabGroup>
   {#each presentLangs as langId}
     <Tab group={requestedLang} name={langId} value={langId}>
-      <a
-        class="block px-4 py-2"
-        href="/word/{title}{paramsToString(currentParams, langId)}"
+      <a class="block px-4 py-2" href="/word/{title}{paramsToString(langId)}"
         >{langs[langId]}</a
       >
     </Tab>
   {/each}
-  <svelte:fragment slot="panel">
-    {#each groupedHets as [dict, hets]}
-      {#if requestedLang === dict.lang}
-        <Word groupedHets={[[dict, hets]]} {title} />
-      {/if}
-    {/each}
-  </svelte:fragment>
 </TabGroup>
