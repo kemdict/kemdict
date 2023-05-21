cf991470c73d098111b5ab86bba7907db624e82a
Kisaragi Hiu <mail@kisaragi-hiu.com>
2023-05-22 01:39:36 +0900

    get skeleton popups to work
    I don't think we need to move to flowbite then.

--- web/src/components/SearchBar.svelte
+++ web/src/components/SearchBar.svelte
@@ -1,21 +1,21 @@
 <script>
-  import clsx from "clsx";
-  import { ListBox, ListBoxItem } from "@skeletonlabs/skeleton";
+  import { popup, ListBox, ListBoxItem } from "@skeletonlabs/skeleton";
   import { onMount } from "svelte";
   export let submitSuffix = "";
   export let initialInput = "";
   export let initialMatchSelection = "prefix";
-  export let showMatchTypes = false;
   export let highlightBtn = false;
   // export let currentLang = "zh_TW";
 
-  let currentMtch = initialMatchSelection;
-  const matchTypes = [
-    { mtch: "prefix", name: "開頭" },
-    { mtch: "suffix", name: "結尾" },
-    { mtch: "contains", name: "包含" },
-    { mtch: "exact", name: "完全符合" },
-  ];
+  let currentMtch = initialMatchSelection || "prefix";
+  const matchTypes = new Map(
+    Object.entries({
+      prefix: "開頭為",
+      suffix: "結尾為",
+      contains: "包含",
+      exact: "完全符合",
+    })
+  );
   onMount(() => {
     window.addEventListener("keydown", (e) => {
       // Following the same approach as
@@ -39,6 +39,28 @@
 <div class="relative mb-2 mt-2">
   <form action="/search{submitSuffix || ''}">
     <div class="flex space-x-2">
+      <div>
+        <button
+          type="button"
+          class="btn variant-filled h-full w-[10ch]"
+          use:popup={{
+            event: "click",
+            target: "combo",
+            placement: "bottom",
+            closeQuery: ".listbox-item",
+          }}>{matchTypes.get(currentMtch)}</button
+        >
+        <div class="card z-10 p-4" data-popup="combo">
+          <ListBox>
+            {#each [...matchTypes] as [mtch, name]}
+              <ListBoxItem bind:group={currentMtch} name="m" value={mtch}>
+                {name}
+              </ListBoxItem>
+            {/each}
+          </ListBox>
+          <div class="arrow bg-surface-100-800-token" />
+        </div>
+      </div>
       <div class="relative flex-grow">
         <input
           id="sbi"
@@ -54,29 +76,10 @@
         </div>
       </div>
       <input
-        class={clsx(
-          "rounded-md p-1 px-2 text-sm font-bold transition",
-          highlightBtn
-            ? [
-                "bg-indigo-500 text-indigo-50 hover:bg-indigo-600",
-                "dark:bg-indigo-500 dark:hover:bg-indigo-400",
-              ]
-            : "btnColor btnBorder"
-        )}
+        class={highlightBtn ? "k-hlbtn" : "k-btn"}
         type="submit"
         value="搜尋"
       />
     </div>
-    {#if showMatchTypes}
-      <div class="max-w-fit">
-        <ListBox>
-          {#each matchTypes as { mtch, name }}
-            <ListBoxItem bind:group={currentMtch} name="m" value={mtch}>
-              {name}
-            </ListBoxItem>
-          {/each}
-        </ListBox>
-      </div>
-    {/if}
   </form>
 </div>
