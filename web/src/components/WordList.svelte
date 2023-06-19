a59bf723d156996ac95eced1235a7497aa67abf4
Kisaragi Hiu <mail@kisaragi-hiu.com>
2023-06-20 00:50:07 +0900

    feat(WordList): a client side search bar

--- web/src/components/WordList.svelte
+++ web/src/components/WordList.svelte
@@ -1,15 +1,29 @@
 <script lang="ts">
+  export let search: boolean;
   export let words: string[];
+
+  let input = "";
 </script>
 
 <div class="not-prose">
+  {#if search}
+    <input
+      type="search"
+      autocomplete="off"
+      placeholder="篩選…"
+      class="text-input"
+      bind:value={input}
+    />
+  {/if}
   <ul class="flex flex-wrap">
-    {#each words as word}
-      <li class="mr-2 py-2">
-        <a class="wordlink" href="/word/{word}">
-          {word}
-        </a>
-      </li>
+    {#each words as word (word)}
+      {#if !search || word.includes(input)}
+        <li class="mr-2 py-2">
+          <a class="wordlink" href="/word/{word}">
+            {word}
+          </a>
+        </li>
+      {/if}
     {/each}
   </ul>
 </div>
