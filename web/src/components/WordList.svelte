374c0537601a3af7cdd832540a96c1ba7673e06c
Kisaragi Hiu <mail@kisaragi-hiu.com>
2023-06-19 23:33:48 +0900

    refactor: use Svelte for WordList to prepare for interactivity

---
+++ web/src/components/WordList.svelte
@@ -0,0 +1,15 @@
+<script lang="ts">
+  export let words: string[];
+</script>
+
+<div class="not-prose">
+  <ul class="flex flex-wrap">
+    {#each words as word}
+      <li class="mr-2 py-2">
+        <a class="wordlink" href="/word/{word}">
+          {word}
+        </a>
+      </li>
+    {/each}
+  </ul>
+</div>
