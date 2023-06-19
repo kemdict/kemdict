2a728590e2bc3917f05085aba8c99ca1b698fca6
Kisaragi Hiu <mail@kisaragi-hiu.com>
2023-06-20 02:09:42 +0900

    feat(WordList): only perform search after IME commits

--- web/src/components/WordList.svelte
+++ web/src/components/WordList.svelte
@@ -2,13 +2,22 @@
   export let search: boolean;
   export let words: string[];
 
-  let input = "";
+  // This is more reliable than binding value then setting input to
+  // value in handlers. The bound value might update too late.
+  let inputElem: HTMLInputElement;
+  let filterString = "";
+  // Paused on compositionstart (IME starts editing), unpaused on
+  // compositionend. This allows the user to continue seeing the
+  // unfiltered list while still typing in their IME.
+  let updatePaused = false;
 
-  function highlight(word: string, input: string) {
-    if (input === "") return word;
+  function highlight(word: string, filterString: string) {
+    if (filterString === "") return word;
     return word
-      .split(input)
-      .join(`<span class="bg-yellow-400 dark:bg-yellow-800">${input}</span>`);
+      .split(filterString)
+      .join(
+        `<span class="bg-yellow-400 dark:bg-yellow-800">${filterString}</span>`
+      );
   }
 </script>
 
@@ -17,17 +26,24 @@
     <input
       type="search"
       autocomplete="off"
-      placeholder="篩選…"
-      class="text-input"
-      bind:value={input}
+      placeholder="搜尋…"
+      class="k-input mb-1 h-10 w-full"
+      bind:this={inputElem}
+      on:input={() => {
+        if (inputElem.value === "" || !updatePaused) {
+          filterString = inputElem.value;
+        }
+      }}
+      on:compositionstart={() => (updatePaused = true)}
+      on:compositionend={() => (updatePaused = false)}
     />
   {/if}
   <ul class="flex flex-wrap">
     {#each words as word (word)}
-      {#if !search || word.includes(input)}
+      {#if !search || filterString === "" || word.includes(filterString)}
         <li class="mr-2 py-2">
           <a class="wordlink" href="/word/{word}">
-            {@html highlight(word, input)}
+            {@html highlight(word, filterString)}
           </a>
         </li>
       {/if}
