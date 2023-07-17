8da369813649fbae4821de8556d01c09237be364
Kisaragi Hiu <mail@kisaragi-hiu.com>
2023-07-17 17:22:23 +0900

    dicts(newwords): store values sorted from new to old

--- dicts/heteronyms-to-sqlite.ts
+++ dicts/heteronyms-to-sqlite.ts
@@ -353,6 +353,7 @@ WHERE "from" LIKE 'kisaragi%'
     words = words.sort((a, b) => a.time - b.time);
   }
   words = sortedUniqBy(words, (v) => `${v.title}${v.from}`);
+  words = words.reverse();
   const newWordStmt = db.prepare(`
 INSERT INTO
   newwords ("title","time","from")
