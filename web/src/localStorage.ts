ef4464a2aed452e219cb14c3a60e661472fd89a8
Kisaragi Hiu <mail@kisaragi-hiu.com>
2024-09-03 21:28:05 +0900

    localStorage: types and docs

--- web/src/localStorage.ts
+++ web/src/localStorage.ts
@@ -2,25 +2,25 @@
  * Helpers for storing serializable values in local storage.
  */
 
-export function lGet(key: string, def?: any): any {
+export function lGet<T>(key: string, def?: T): T | undefined {
   const s = window.localStorage.getItem(key);
   if (typeof s === "undefined") {
     return def;
   }
-  return JSON.parse(s);
+  return typeof s === "string" && JSON.parse(s);
 }
 
-export function lSet(key: string, value: any) {
+export function lSet<T>(key: string, value: T) {
   window.localStorage.setItem(key, JSON.stringify(value));
 }
 
-export function lUpdate(key: string, updater: (value: any) => void) {
+export function lUpdate<T>(key: string, updater: (value: T | undefined) => T) {
   lSet(key, updater(lGet(key)));
 }
 
 /** Push `value` to the array stored in `key`. */
-export function lPush(key: string, value: any) {
-  lUpdate(key, (arr) => {
+export function lPush<T>(key: string, value: T) {
+  lUpdate<T[]>(key, (arr) => {
     if (Array.isArray(arr)) {
       // Put it in "front" without duplicating
       const s = new Set(arr);
@@ -33,6 +33,11 @@ export function lPush(key: string, value: any) {
   });
 }
 
+/**
+ * Save OBJ into a history localStorage.
+ * Given a type of, say, "word", this would save into an array under the
+ * "wordHistory" key.
+ */
 export function saveHistory(type: string, obj: any) {
   if (typeof window !== "undefined" && obj) {
     lPush(`${type}History`, obj);
