486c34fe790430cffc26b7ef23ee2d0b90431462
Kisaragi Hiu <mail@kisaragi-hiu.com>
2025-05-04 14:57:11 +0900

    work around "%" failure
    This at least means in dev mode on pages using this api route, the error
    will not bubble up into the page itself. (In production it already doesn't.)

--- web/src/lib/client.ts
+++ web/src/lib/client.ts
@@ -1,5 +1,14 @@
 export async function clientGetCompletions(prefix: string) {
   if (prefix.trim() === "") return [];
-  const res = await fetch(`/api/compl/prefix/${prefix}`);
-  return res.json() as Promise<string[]>;
+  // work around Astro middleware failing for some URLs
+  // Namely, it assumes the input URL can be successfully decoded with
+  // `decodeURI`, but if I give it "/%" decodeURI will fail, causing an
+  // error.
+  // Astro 5.7.2, @astrojs/node 9.2.0
+  // This is an Astro issue because any user can type /api/compl/prefix/% into
+  // the API and trigger the error. It doesn't bring down the server, but
+  // there's nothing I can do for the error.
+  const res = await fetch(encodeURI(`/api/compl/prefix/${prefix}`));
+  const text = await res.text();
+  return JSON.parse(text) as string[];
 }
