1c0349b58df8a380ba90dd08b255e663a0e53078
Kisaragi Hiu <mail@kisaragi-hiu.com>
2025-05-27 22:41:18 +0900

    mobile: work in progress home page

--- mobile/src/components/IndexPage.tsx
+++ mobile/src/components/IndexPage.tsx
@@ -1,5 +1,43 @@
-import { Button } from "@mui/material";
+import {
+  Container,
+  List,
+  ListItemIcon,
+  ListItemText,
+  ListItemButton,
+  Typography,
+} from "@mui/material";
+import SearchAppBar from "./SearchAppBar.tsx";
+import DescriptionIcon from "@mui/icons-material/Description";
 
-export default function IndexPage() {
-  return <Button variant="contained">Hello</Button>;
+export default function IndexPage({ title }: { title: string }) {
+  return (
+    <>
+      <SearchAppBar title={title} />
+      <Container>
+        <Typography sx={{ mt: 2, mb: 0 }} variant="h6" component="div">
+          索引
+        </Typography>
+        <List dense={false}>
+          <ListItemButton>
+            <ListItemIcon>
+              <DescriptionIcon />
+            </ListItemIcon>
+            <ListItemText primary="首字索引" />
+          </ListItemButton>
+          <ListItemButton>
+            <ListItemIcon>
+              <DescriptionIcon />
+            </ListItemIcon>
+            <ListItemText primary="部首索引" />
+          </ListItemButton>
+          <ListItemButton>
+            <ListItemIcon>
+              <DescriptionIcon />
+            </ListItemIcon>
+            <ListItemText primary="新詞列表" />
+          </ListItemButton>
+        </List>
+      </Container>
+    </>
+  );
 }
