40359d7bc2698f29293e7c7920da6098e6a20463
Kisaragi Hiu <mail@kisaragi-hiu.com>
2026-05-10 17:50:43 +0900

    favorites listing page
    just like the histories page

---
+++ web/src/pages/favorites/_Favorites.svelte
@@ -0,0 +1,49 @@
+<script lang="ts">
+  import WordList from "$src/components/WordList.svelte";
+  import { lGet } from "$src/localStorage";
+  import { localStorageStore } from "@skeletonlabs/skeleton";
+  import type { Writable } from "svelte/store";
+
+  const favorites: Writable<string[]> = localStorageStore("favorites", []);
+  function guardGet(key: string) {
+    const value = lGet(key);
+    if (Array.isArray(value)) {
+      return value;
+    } else {
+      return [];
+    }
+  }
+  let copyFavorites: HTMLButtonElement;
+</script>
+
+<div class="sm:mt-4 sm:grid sm:grid-cols-2">
+  <div>
+    <h2 class="mb-2 text-xl font-bold max-sm:mt-4">收藏項目</h2>
+    <div class="mb-2 space-x-1">
+      <button
+        bind:this={copyFavorites}
+        disabled={$favorites.length === 0}
+        onclick={() => {
+          navigator.clipboard.writeText(
+            JSON.stringify($favorites.map((str) => decodeURI(str))),
+          );
+          const oldInnerText = copyFavorites.innerText;
+          copyFavorites.innerText = "已複製！";
+          setTimeout(() => {
+            copyFavorites.innerText = oldInnerText;
+          }, 1000);
+        }}
+        class="variant-filled btn">匯出收藏詞彙</button
+      >
+    </div>
+    {#if $favorites.length > 0}
+      <WordList
+        words={[...$favorites].map(decodeURIComponent).reverse()}
+        search={false}
+        deleter={(word) => {
+          $favorites = $favorites.filter((val) => decodeURIComponent(val) !== word)
+        }}
+      ></WordList>
+    {/if}
+  </div>
+</div>
