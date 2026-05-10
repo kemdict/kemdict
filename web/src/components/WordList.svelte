e13b7ba3ecc054d604a072302b61e13217b4e619
Kisaragi Hiu <mail@kisaragi-hiu.com>
2026-05-10 21:42:51 +0900

    use ionicons icon for delete button

--- web/src/components/WordList.svelte
+++ web/src/components/WordList.svelte
@@ -37,9 +37,29 @@
             {@html highlight(word, filterString)}
           </a>
           {#if deleter}
-            <button
-              onclick={deleter(word)}
-              class="variant-filled btn px-2 py-1">☓</button>
+            <!-- ionicons close-outline -->
+            <button onclick={deleter(word)} class="variant-filled btn px-2 py-1"
+              ><svg
+                class="h-5 w-5"
+                xmlns="http://www.w3.org/2000/svg"
+                width="512"
+                height="512"
+                viewBox="0 0 512 512"
+                ><line
+                  x1="368"
+                  y1="368"
+                  x2="144"
+                  y2="144"
+                  style="fill:none;stroke:#000;stroke-linecap:round;stroke-linejoin:round;stroke-width:32px"
+                /><line
+                  x1="368"
+                  y1="144"
+                  x2="144"
+                  y2="368"
+                  style="fill:none;stroke:#000;stroke-linecap:round;stroke-linejoin:round;stroke-width:32px"
+                /></svg
+              ></button
+            >
           {/if}
         </li>
       {/if}
