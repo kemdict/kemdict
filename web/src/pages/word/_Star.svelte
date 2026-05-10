48d8783b8fef39209ef7f3c68d505288a55f67ac
Kisaragi Hiu <mail@kisaragi-hiu.com>
2026-05-10 20:18:54 +0900

    favorites button

---
+++ web/src/pages/word/_Star.svelte
@@ -0,0 +1,25 @@
+<script lang="ts">
+  import WordList from "$src/components/WordList.svelte";
+  import { lGet } from "$src/localStorage";
+  import { localStorageStore } from "@skeletonlabs/skeleton";
+  import type { Writable } from "svelte/store";
+
+  // both are encoded
+  const favorites: Writable<string[]> = localStorageStore("favorites", []);
+  const word = (new URL(globalThis.location?.href)).pathname.replace("/word/", "")
+
+  function addHandler() {
+    $favorites.push(word)
+  }
+  function removeHandler() {
+    // word and words in the array are both encoded
+    $favorites = $favorites.filter((val) => val !== word)
+  }
+  let starred = $derived($favorites.includes(word))
+</script>
+
+<button
+  class="text-lg"
+  disabled={!globalThis.location}
+  onclick={starred ? removeHandler : addHandler}>{starred ? "★" : "☆"}</button
+>
