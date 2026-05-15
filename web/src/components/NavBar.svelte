1bafebe30c19e96c3373c616e51606306ce93f04
Kisaragi Hiu <mail@kisaragi-hiu.com>
2026-05-16 08:48:54 +0900

    Start adding a navbar

---
+++ web/src/components/NavBar.svelte
@@ -0,0 +1,29 @@
+<script lang="ts">
+  import NavBarButton from "$src/components/NavBarButton.svelte";
+  import IconHomeOutline from "$src/components/IconHomeOutline.svelte";
+  import IconHome from "$src/components/IconHome.svelte";
+  import IconStarOutline from "$src/components/IconStarOutline.svelte";
+  import IconSearchOutline from "$src/components/IconSearchOutline.svelte";
+  import IconHistory from "$src/components/IconHistory.svelte";
+
+  const { class: klass } = $props();
+</script>
+
+<div class={klass}>
+  <NavBarButton href="/" text="首頁">
+    <IconHome slot="active" />
+    <IconHomeOutline slot="normal" />
+  </NavBarButton>
+  <NavBarButton href="/search" text="搜尋">
+    <IconSearchOutline slot="active" />
+    <IconSearchOutline slot="normal" />
+  </NavBarButton>
+  <NavBarButton href="/favorites" text="收藏">
+    <IconStarOutline slot="active" />
+    <IconStarOutline slot="normal" />
+  </NavBarButton>
+  <NavBarButton href="/history" text="紀錄">
+    <IconHistory slot="active" />
+    <IconHistory slot="normal" />
+  </NavBarButton>
+</div>
---
+++ web/src/components/NavBarButton.svelte
@@ -0,0 +1,26 @@
+<script lang="ts">
+  export let text: string;
+  export let href: string;
+  $: active =
+    globalThis?.location?.href !== undefined &&
+    new URL(location.href).pathname === href;
+
+  console.log({
+    active,
+    href,
+    locationHrefPathname:
+      globalThis?.location?.href !== undefined &&
+      new URL(location.href).pathname,
+  });
+</script>
+
+<a {href} class="flex flex-col items-center justify-center gap-1 text-center">
+  <div class="h-6 w-6">
+    {#if active}
+      <slot name="active" />
+    {:else}
+      <slot name="normal" />
+    {/if}
+  </div>
+  <div class="text-xs">{text}</div>
+</a>
