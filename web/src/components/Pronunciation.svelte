72d6d739417533a847361d32d1a1de5aaf095ebd
Kisaragi Hiu <mail@kisaragi-hiu.com>
2026-05-18 05:18:29 +0900

    try another way to copy as the button doesn't work on my phone

--- web/src/components/Pronunciation.svelte
+++ web/src/components/Pronunciation.svelte
@@ -5,7 +5,6 @@
   }
 
   let { id = undefined, children }: Props = $props();
-  let Pn: HTMLButtonElement;
 </script>
 
-<button title="複製" bind:this={Pn} onclick={() => navigator.clipboard.writeText(Pn.innerText)} class="hover:opacity-75 active:opacity-90 fade cursor-pointer my-2 text-gray-800 dark:text-gray-200 text-lg" {id}>{@render children?.()}</button>
+<button title="複製" onclick={(e) => navigator.clipboard.writeText(e.target.innerText)} class="hover:opacity-75 active:opacity-90 fade cursor-pointer my-2 text-gray-800 dark:text-gray-200 text-lg" {id}>{@render children?.()}</button>
