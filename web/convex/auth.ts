a9e0bcb396f58138f363fd4737f1939ce553c387
Kisaragi Hiu <mail@kisaragi-hiu.com>
2026-05-20 05:58:58 +0900

    No, I don't actually plan to end up with both React and Svelte on the same page

--- web/convex/auth.ts
+++
@@ -1,5 +0,0 @@
-import { convexAuth } from "@convex-dev/auth/server";
-
-export const { auth, signIn, signOut, store, isAuthenticated } = convexAuth({
-  providers: [],
-});
--- web/convex/schema.ts
+++
@@ -1,8 +0,0 @@
-import { defineSchema } from "convex/server";
-import { authTables } from "@convex-dev/auth/server";
-
-const schema = defineSchema({
-  ...authTables,
-});
-
-export default schema;
