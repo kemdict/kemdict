ffb222f55af7c085b526a4f51d34925523c7e9c1
Kisaragi Hiu <mail@kisaragi-hiu.com>
2026-05-20 08:29:47 +0900

    give up convex
    I don't think it's possible to get convex + better-auth + astro + svelte
    working without inventing everything we need ourselves. Just use
    better-auth + local sqlite instead.

---
+++ web/src/components/UserBox.svelte
@@ -0,0 +1,36 @@
+<script lang="ts">
+  const loading = !globalThis.location;
+  const isDark = false;
+  import { authClient } from "$lib/auth-client";
+  const session = authClient.useSession();
+  const klass =
+    "btn h-5 bg-yellow-300 p-1 hover:bg-yellow-400 dark:bg-orange-900 dark:hover:bg-orange-800";
+</script>
+
+{#if loading}
+  <div class={klass}></div>
+{:else if $session.data}
+  <button
+    class={klass}
+    type="button"
+    title="logout"
+    aria-label="logout"
+    onclick={() => {
+      authClient.signOut();
+    }}
+  >
+    logged in as {$session.data.user.name}
+  </button>
+{:else}
+  <button
+    class={klass}
+    type="button"
+    title="login"
+    aria-label="login"
+    onclick={() => {
+      authClient.signIn.social({ provider: "github" });
+    }}
+  >
+    sign in with google
+  </button>
+{/if}
