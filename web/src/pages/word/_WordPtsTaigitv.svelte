4bd1579dab6e9ef53754951dac46f6e66627b190
Kisaragi Hiu <mail@kisaragi-hiu.com>
2026-05-25 23:50:01 +0900

    try to fix pts-taigitv multiple pronunciations being bunched up together

--- web/src/pages/word/_WordPtsTaigitv.svelte
+++ web/src/pages/word/_WordPtsTaigitv.svelte
@@ -6,14 +6,19 @@
 
 {#each heteronyms as het}
   <h1>{het.title}</h1>
-  {#each het.props?.pn || [] as pn}
-    <Pronunciation>{pn}</Pronunciation>
-  {/each}
+  <div class="space-x-2">
+    {#each het.props?.pn || [] as pn, i}
+      {#if i !== 0}<span>/</span>{/if}
+      <Pronunciation>{pn}</Pronunciation>
+    {/each}
+  </div>
   {#if het.props?.tags}
     <Property key="標籤">
       {#each het.props.tags as tag}
         <!-- <a href={`https://taigiwords.taigitv.org.tw/zh_tw/dic?orbithashtag=${tag.title}`}>#{tag.title}</a> -->
-        <a class="mr-1" href={`/search?q=%23${tag.title}&m=exact`}>#{tag.title}</a>
+        <a class="mr-1" href={`/search?q=%23${tag.title}&m=exact`}
+          >#{tag.title}</a
+        >
       {/each}
     </Property>
   {/if}
