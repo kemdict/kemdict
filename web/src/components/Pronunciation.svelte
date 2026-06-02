743ee260d55e485f2758611336c1d066c3c8f1f7
Kisaragi Hiu <mail@kisaragi-hiu.com>
2026-06-02 11:50:29 +0900

    Pronunciation: messy copy feedback
    this also addresses a warning about e.target's type

--- web/src/components/Pronunciation.svelte
+++ web/src/components/Pronunciation.svelte
@@ -1,10 +1,26 @@
 <script lang="ts">
   interface Props {
     id?: string | undefined;
-    children?: import('svelte').Snippet;
+    children?: import("svelte").Snippet;
   }
 
   let { id = undefined, children }: Props = $props();
+
+  let self: HTMLButtonElement;
 </script>
 
-<button title="複製" onclick={(e) => navigator.clipboard.writeText(e.target.innerText)} class="hover:opacity-75 active:opacity-90 fade cursor-pointer my-2 text-gray-800 dark:text-gray-200 text-lg" {id}>{@render children?.()}</button>
+<button
+  bind:this={self}
+  title="複製"
+  onclick={() => {
+    navigator.clipboard.writeText(self.innerText);
+    // eh...
+    const oldInnerHTML = self.innerHTML;
+    self.append(" - 已複製！");
+    setTimeout(() => {
+      self.innerHTML = oldInnerHTML;
+    }, 1000);
+  }}
+  class="hover:opacity-75 active:opacity-90 fade cursor-pointer my-2 text-gray-800 dark:text-gray-200 text-lg"
+  {id}>{@render children?.()}</button
+>
