eabb45f68ab918b28da4d8c68fa3a95c120fb338
Kisaragi Hiu <mail@kisaragi-hiu.com>
2026-06-03 12:29:36 +0900

    get rid of the too many languages hack
    we'll figure it out if we actually end up with more than like 5 languages

--- web/src/pages/word/_WordTabs.svelte
+++ web/src/pages/word/_WordTabs.svelte
@@ -38,42 +38,11 @@
 </script>
 
 <TabGroup padding="">
-  {#each presentLangs.slice(0, 7) as langId}
+  {#each presentLangs as langId}
     <Tab bind:group={currentTab} name={langId} value={langId}>
       <div class="block px-4 py-2">{langs[langId as LangId]}</div>
     </Tab>
   {/each}
-  {#if presentLangs.length > 7}
-    <div class="ml-2 flex items-center">
-      <button
-        class="btn btn-sm variant-filled"
-        type="button"
-        title="其他語言"
-        use:popup={{
-          event: "click",
-          target: "popupLangs",
-          placement: "bottom",
-          closeQuery: ".listbox-item",
-        }}
-        >{presentLangs.indexOf(requestedLang) >= 7
-          ? langs[requestedLang as LangId]
-          : ""}…</button
-      >
-      <div class="card z-10 p-4 shadow-xl" data-popup="popupLangs">
-        <ListBox padding="">
-          {#each presentLangs.slice(7) as langId}
-            <ListBoxItem group={requestedLang} name="lang" value={langId}>
-              <a
-                class="block px-4 py-2"
-                href="/word/{title}{paramsToString(langId)}">{langs[langId]}</a
-              >
-            </ListBoxItem>
-          {/each}
-        </ListBox>
-        <div class="arrow bg-surface-100-800-token"></div>
-      </div>
-    </div>
-  {/if}
   <svelte:fragment slot="panel">
     <div class="relative">
       <div class="absolute right-0 top-0">
