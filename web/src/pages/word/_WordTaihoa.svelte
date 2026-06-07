325671ea01353de6f64e8c5b509bea99d96c369b
Kisaragi Hiu <mail@kisaragi-hiu.com>
2026-06-08 04:13:36 +0900

    Add rendering for 台文華文線頂辭典

---
+++ web/src/pages/word/_WordTaihoa.svelte
@@ -0,0 +1,23 @@
+<script lang="ts">
+  import type { Heteronym } from "common";
+  interface Props {
+    heteronyms: Heteronym[];
+  }
+  let { heteronyms }: Props = $props();
+  import Pronunciation from "$src/components/Pronunciation.svelte";
+  import Property from "$src/components/Property.svelte";
+  import { taigiTitle } from "common";
+
+  // let allSame = heteronyms.map((het) => het.props.kip);
+</script>
+
+{#each heteronyms as het}
+  <h1>{taigiTitle(het)}</h1>
+  <Pronunciation>{het.props.kip}</Pronunciation>
+  {#if het.props.kipOthers}
+    <Property key="其他講法">
+      <Pronunciation>{het.props.kipOthers}</Pronunciation>
+    </Property>
+  {/if}
+  <Property key="華語" value={het.props.zh} html={true}></Property>
+{/each}
